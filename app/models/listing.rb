c4b8742937f261014615029e43930a5f2f4fee23
rubyhpsek <sensorbrain@gmail.com>
2014-11-09 02:47:07 +0000

    typo and change again

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,7 @@
 class Listing < ActiveRecord::Base
-
-
 	if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-  else
+   else
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
         :storage => :dropbox,
         :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
